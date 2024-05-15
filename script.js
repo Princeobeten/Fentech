514f70e03d87967b1025e2072ee96c2926471cdf
prince obeten <princeobeten56@gmail.com>
2024-05-15 12:42:13 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
+
+function toggleMenu() {
+    const sidebarMenu = document.getElementById('sidebar-menu');
+    sidebarMenu.classList.toggle('show');
+}
